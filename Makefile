60997c231632e45b43fdb37cd099319d45a39640
süleyman akkuş <sakkus42@DESKTOP-VQRL46J>
2023-03-31 22:19:34 +0300

    31.03

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 NAME = minishell
 CC = gcc
-FLAGS = -Wall -Wextra -Werror -I./lib/readline/include
-LDFLAGS = -L./lib/readline/lib -lreadline
+FLAGS = -Wall -Wextra -Werror 
+LDFLAGS = -lreadline
+LIBFT = libft/libft.a
 SRC = src/main.c \
 	  src/builtcmd.c \
 	  src/input_parser.c\
@@ -9,8 +10,11 @@ SRC = src/main.c \
 
 all: $(NAME)
 
-$(NAME): src/*.c
-	$(CC) $(FLAGS) $(LDFLAGS) $(SRC) libft/libft.a -o $(NAME)
+$(NAME): src/*.c $(LIBFT)
+	$(CC) $(FLAGS) $(SRC) $(LIBFT) $(LDFLAGS)  -o $(NAME)
+
+$(LIBFT):
+	make -C libft/
 
 clean:
 	rm -rf minishell
--- src/input_parser.c
+++ src/input_parser.c
@@ -1,9 +1,52 @@
 #include "../include/minishell.h"
 
+struct input_parser
+{
+	char	**quote;
+}parser;
+
+int	is_blank_line(char *line)
+{
+	if (!*line)
+		return (1);
+	if (*line == ' ')
+		return (is_blank_line(line + 1));
+	else
+		return (0);
+}
+
 void	input_parser(void)
 {
-	// t_data.inp_parser = ft_split(t_data.input, ' ');
-	// int i = 0;
-	// while (t_data.inp_parser[i])
-	// 	printf("%s\n", t_data.inp_parser[i++]);
+	if (t_data.input[0] != '\'' && t_data.input[0] != '"')
+		return ;
+	parser.quote = ft_split(t_data.input, t_data.input[0]);
+	t_data.inp_parser = malloc(sizeof(char **) * 999);
+	int i = 0;
+	if (parser.quote[i] && parser.quote[i + 1])
+	{
+		if (parser.quote[i + 1][0] != ' ')
+			t_data.inp_parser[i] = ft_strjoin(parser.quote[i], parser.quote[i + 1]);
+		else
+		{
+			i = 0;
+			while (parser.quote[i])
+			{
+				if (is_blank_line(parser.quote[i]))
+				{
+					t_data.inp_parser[i] = ft_strdup(" ");
+				}
+				else
+					t_data.inp_parser[i] = ft_strdup(parser.quote[i]);
+				i++;
+			}	
+		}
+	}
+	else
+	{
+			t_data.inp_parser[i] = ft_strdup(parser.quote[i]);
+			i++;
+	}
+	i = 0;
+	while (t_data.inp_parser[i])
+		printf("%s\n", t_data.inp_parser[i++]);
 }
